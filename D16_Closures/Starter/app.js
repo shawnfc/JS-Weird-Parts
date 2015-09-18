d3f416b83172350a4bcc8fa3ead9ca63ed058221
Shawn Conrad <shawn_conrad@homedepot.com>
2015-09-18 15:10:18 -0400

    Finished #50 (call, apply, bind).

--- D16_Closures/Starter/app.js
+++ D16_Closures/Starter/app.js
@@ -1,17 +1,44 @@
-function makeGreeting (language) {
-    return function (firstname, lastname) {
-        if(language === 'en') {
-            console.log('Hello ' + firstname + ' ' + lastname);
-        }
+var person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName: function() {
 
-        if(language === 'es') {
-            console.log('Hola ' + firstname + ' ' + lastname);
-        }
+        var fullname = this.firstName + ' ' + this.lastName;
+
+        return fullname;
     }
+};
+
+var logName = function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+    console.log('Arguments: ' + lang1 + " " + lang2);
+    console.log('=================');
+};
+
+var logPersonName = logName.bind(person);
+
+logPersonName('en');
+
+logName.call(person, 'es', 'en');
+
+logName.apply(person, ['es', 'en']);
+
+
+
+var person2 = {
+    firstName: 'Jane',
+    lastName: 'Doe'
+};
+
+
+//Function Borrowing
+console.log(person.getFullName.apply(person2));
+
+//Function Currying
+function multiply (a, b) {
+   return a * b;
 }
 
-var greetEnglish = makeGreeting('en');
-var greetSpanish = makeGreeting('en');
+var multiplyByTwo  = multiply.bind(this, 2);
 
-greetEnglish('Shawn', 'Conrad');
-greetSpanish('Freshco', 'Flows');
+console.log(multiplyByTwo(8));
