00f26fabcf3d0f0906c3b1a69093157877962c62
Shawn Conrad <shawn_conrad@homedepot.com>
2015-10-01 09:00:16 -0400

    completed #59.

--- F1_FunctionConstructorsNewHistory/Starter/app.js
+++ F1_FunctionConstructorsNewHistory/Starter/app.js
@@ -3,8 +3,18 @@ function Person (firstname, lastname) {
     this.lastname = lastname;
 }
 
+Person.prototype.getFullName = function () {
+    return this.firstname + " " + this.lastname;
+};
+
 var john = new Person("Shawn", "Price");
 console.log(john);
 
 var jane = new Person("Jane", "Simpson");
 console.log(jane);
+
+Person.prototype.getFormalFullName = function(){
+  return this.lastname + ", " + this.firstname;
+};
+
+console.log(jane.getFormalFullName());
