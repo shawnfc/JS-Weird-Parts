8f590331cf7fd4a959f69a963cca8646ccd17719
Shawn <mrshawnconrad@gmail.com>
2016-06-12 03:39:41 +0000

    started course again to review

--- app.js
+++ app.js
@@ -1,43 +1,10 @@
-//functional programming
-function mapForEach (arr, fn) {
+var a = "Master Ninja";
 
-    var newArr = [];
+console.log(a);
 
-    for(var i = 0; i < arr.length; i++) {
-
-        newArr.push(
-            fn(arr[i])
-        );
-
-    }
-
-    return newArr;
+if (a === undefined) {
+    console.log('a is undefined')
 }
-
-
-//the long way
-var arr1 = [1, 2, 3];
-
-var arr2 = mapForEach(arr1, function(item) {
-    return item * 3;
-});
-
-console.log(arr1);
-
-console.log(arr2);
-
-console.log('--------------');
-
-var arr3 = mapForEach(arr1, function(item) {
-    return item > 2;
-});
-
-console.log(arr3);
-
-
-var checkPastLimit = function(limiter, item) {
-    return item > limiter;
-};
-
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 2));
-console.log(arr4);
+else {
+    console.log('a is defined!')
+}
