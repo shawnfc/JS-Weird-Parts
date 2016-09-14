eee013a99fddbe2a07870e4e7822eda593d83638
shawnfc <shawn@shawnconrad.com>
2016-09-14 11:07:07 -0400

    call apply and bind

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-(function greet(name) {
-    console.log(name);
-})("Mr");
+var person = {
+    firstname: "John",
+    lastname: "Doe",
+    getFullName: function () {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
 
-var greetFunc = function (name) {
-    console.log(name);
-}("Shawn");
+var logName = function(lang1, lang2) {
+    console.log('Logged: ' + this.getFullName());
+    console.log("Arguments: " + lang1 + ' ' + lang2);
+    console.log('-----------------')
+};
 
-(function(global, name, age, sport) {
-    var greeting = "Please welcome";
-    global.greeting = "Ladies and gentlemen this is";
-    console.log(greeting + ' ' + name + ' who is ' + age + ' and loves ' + sport + '.');
-}(window, 'Freshco', 47, 'Tennis'));
+var logPersonName = logName.bind(person);
 
-console.log(greeting + ' Shawn Freshco!');
+logPersonName("Shawn", "Conrad");
+
+logName.call(person, 'Spanish', 'English');
+
+logName.apply(person, ['French', 'Italian']);
+
+//function borrowing
+var person2 = {
+    firstname: "Jane",
+    lastname: "Doe"
+};
+
+console.log(person.getFullName.apply(person2));
+
+
+//function currying: creating a copy of a function with preset parameters
+function multiply(a, b) {
+    return a * b;
+};
+
+var multiplyByTwo = multiply.bind(this, 2); //set 2 as the first parameter
+
+console.log(multiplyByTwo(4));
+
+
+
+
+
+
+
+
+
